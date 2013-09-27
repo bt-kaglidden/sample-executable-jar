7715eb50221044ca80911c3aea4dcdaf5d09e594
Ken Glidden <ken@basistech.com>
2013-09-27 12:02:01 -0400

    Updates to test for ETROG-984

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,30 +1,18 @@
-import sample.RSELemmatizerSample;
 import sample.RSETokenizerSample;
-
+import tagger.Tagger;
 import java.io.IOException;
-import java.util.Arrays;
 
-/**
- * User: luke
- * Date: 7/05/13
- * Time: 6:50 AM
- */
+
 public class EntryPoint {
     public static void main(String[] args) throws IOException {
-        RSELemmatizerSample lemmatizerSample = new RSELemmatizerSample();
-        try {
-            lemmatizerSample.doIt(new String[] {"/opt/rse-1.11.0", "eng", "/Users/ken/work/sample-executable-jar/src/main/resources/input_eng.txt", "/Users/ken/tmp/out-lemma-eng.txt"});
-            lemmatizerSample.doIt(new String[] {"/opt/rse-1.11.0", "swe", "/Users/ken/work/sample-executable-jar/src/main/resources/input_swe.txt", "/Users/ken/tmp/out-lemma-swe.txt"});
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("Testing RSE 1.11...");
         RSETokenizerSample tokenizerSample = new RSETokenizerSample();
         try {
-            tokenizerSample.doIt(new String[] {"/opt/rse-1.11.0", "eng", "/Users/ken/work/sample-executable-jar/src/main/resources/input_eng.txt", "/Users/ken/tmp/out-token-eng.txt"});
-            tokenizerSample.doIt(new String[] {"/opt/rse-1.11.0", "swe", "/Users/ken/work/sample-executable-jar/src/main/resources/input_swe.txt", "/Users/ken/tmp/out-token-swe.txt"});
+            tokenizerSample.doIt(new String[] {"/opt/rse-1.11.0", "eng", "/Users/ken/work/sample-executable-jar/src/main/resources/input_eng.txt", "/Users/ken/tmp/out-rse1-11-token-eng.txt"});
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        System.out.println("Testing RSE 2.0...");
+        Tagger.main(new String[] {"/Users/ken/tmp/rse-2.0-jarjar", "eng", "/Users/ken/work/sample-executable-jar/src/main/resources/input_eng.txt", "/Users/ken/tmp/out-rse2-0-tagger-eng.txt"});
     }
 }
